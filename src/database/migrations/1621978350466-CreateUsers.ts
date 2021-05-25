0860b7e21f2de8b0d7b22c9a3ce4d96ec8cf9810
Domuserk <rgdesenhos321@gmail.com>
2021-05-25 18:33:22 -0300

    fix importCategories

---
+++ src/database/migrations/1621978350466-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1621978350466 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
