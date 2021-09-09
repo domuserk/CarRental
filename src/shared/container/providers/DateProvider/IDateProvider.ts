dbe28122a1c3e3c1ab174bccb2fa765d4b14825a
domuserk <rgdesenhos321@gmail.com>
2021-09-09 18:46:20 -0300

    usecase devolution car

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -1,7 +1,8 @@
 interface IDateProvider {
   compareInHours(start_date: Date, end_date: Date): number
-  convertToUTC(date: Date)
+  convertToUTC(date: Date);
   dateNow(): Date;
+  compareInDays(start_date: Date, end_date: Date): number;
 }
 
 export { IDateProvider }
